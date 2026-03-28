54d0e21137529ebb05d6049afe62b3fcf7b4f11c
harsh1214 <harsh.yadav.1412@gmail.com>
2026-03-28 23:19:43 +0530

    Test 01

--- app/main.py
+++ app/main.py
@@ -1,27 +1,28 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-from app.app import search_query
-from pathlib import Path
+# from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.responses import FileResponse
+# from app.app import search_query
+# from pathlib import Path
 
 app = FastAPI()
-path = Path(__file__).resolve().parent.parent
+# path = Path(__file__).resolve().parent.parent
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://multi-modal-rag-system-rpli.onrender.com/"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["https://multi-modal-rag-system-rpli.onrender.com/"],
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 @app.get("/")
 async def root():
-    return FileResponse(f"{path}/static/index.html")
+    # return FileResponse(f"{path}/static/index.html")
+    return "Hello World!"
 
-@app.get("/search")
-async def search(query: str):
-    result = search_query(query)
-    return {
-        "answer": " ".join(result[:2]),
-        "results": [{"text": r} for r in result]
-    }
+# @app.get("/search")
+# async def search(query: str):
+#     result = search_query(query)
+#     return {
+#         "answer": " ".join(result[:2]),
+#         "results": [{"text": r} for r in result]
+#     }
