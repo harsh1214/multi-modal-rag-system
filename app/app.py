ddcdc43a51ab018090ec869785194be70c5eac07
harsh1214 <harsh.yadav.1412@gmail.com>
2026-03-29 16:34:32 +0530

    Updated 05

--- app/app.py
+++ app/app.py
@@ -5,18 +5,18 @@ from app.vector_db import QdrantManager
 pipeline = EmbeddingPipeline()
 db = QdrantManager()
 
-
-def search_query(query_text: str = "What is DL?"):
+def load_data(path: str = "./data"):
     if not db.collection_exist():
-        loader = DocumentLoader("./data")
+        loader = DocumentLoader(path)
         data = loader.load()
         embedded_data = pipeline.process(data)
         vector_size = len(embedded_data[0]["embedding"])
         db.create_collection(vector_size)
         db.upsert(embedded_data)
+    return loader.load()
 
+def search_query(query_text: str = "What is DL?"):
     query = pipeline.model.encode([query_text], normalize_embeddings=True, show_progress_bar=False)[0]
-
     results = db.search(query.tolist(), top_k=5)
 
     result = []
