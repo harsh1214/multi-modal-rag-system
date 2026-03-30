19963f7b01d299c49d4e8fb6003d7a15746c7727
harsh1214 <harsh.yadav.1412@gmail.com>
2026-03-30 16:55:02 +0530

    Test 04

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-# from app.app import search_query
+from app.app import search_query
 from pathlib import Path
 
 app = FastAPI()
@@ -25,9 +25,8 @@ async def root():
 
 @app.get("/search")
 async def search(query: str):
-    # result = search_query(query)
+    result = search_query(query)
     return {
-        # "answer": " ".join(result[:2]),
-        "answer": "Working",
-        # "results": [{"text": r} for r in result]
+        "answer": " ".join(result[:2]),
+        "results": [{"text": r} for r in result]
     }
